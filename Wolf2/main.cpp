3e1f134617e1dadba4ef1d107bd75534b8e2d0ec
ZhHong <371816216@qq.com>
2015-11-05 21:59:33 +0800

    create obj thread

--- Wolf2/main.cpp
+++ Wolf2/main.cpp
@@ -7,15 +7,22 @@
 #include "Utils.h"
 
 int main(){
-	std::map<int, std::thread> wolfmap;
+	std::map<int, Wolf> wolfmap;
 	for (int i = 1000; i < 1004; i++){
 		//http://blog.csdn.net/zhq651/article/details/8491277
 		//at the end only 3 1003 run
-		std::thread td1(std::bind(&Wolf::mainloop, &Wolf(i)));
-		td1.detach(); //join wait for complete detach not wait
-		//wolfmap.insert(std::pair<int, std::thread>(i, td1));
-		Utils::print(std::to_string(i)+" times");
+		//std::thread td1(std::bind(&Wolf::mainloop, &Wolf(i)));
+		//td1.detach(); //join wait for complete detach not wait
+		////wolfmap.insert(std::pair<int, std::thread>(i, td1));
+		//Utils::print(std::to_string(i)+" times");
+		wolfmap.insert(std::pair<int, Wolf>(i, Wolf(i)));
 	}
+	std::map<int, Wolf>::iterator it;
+	for (it = wolfmap.begin(); it != wolfmap.end(); ++it){
+		std::thread td1(std::bind(&Wolf::mainloop, &it->second));
+		td1.detach();
+	}
+
 
 	system("pause");
 }
